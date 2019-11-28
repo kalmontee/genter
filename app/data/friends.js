8b388c35706d8ec28e51f542d78e96f2ac73ac29
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-28 14:22:12 -0500

    Added more friends on the API list.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,83 @@
-var user = {
-    "name": "kelvin",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}
+const userData = [{
+        "name": "The Weeknd",
+        "photo": "https://www.rap-up.com/app/uploads/2019/11/the-weeknd-mg.jpg",
+        "scores": [
+            2,
+            4,
+            1,
+            3,
+            5,
+            3,
+            4,
+            5,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "The Rock",
+        "photo": "https://compote.slate.com/images/e106a9e3-78e9-4bce-9c5d-2b8dcd7ada0f.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Kendall Jenner",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kendall-jenner-attends-the-amfar-cannes-gala-2019-at-hotel-news-photo-1151245970-1562528122.jpg?crop=1.00xw:0.667xh;0,0.144xh&resize=480:*",
+        "scores": [
+            1,
+            5,
+            4,
+            4,
+            2,
+            3,
+            1,
+            2,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Alicia Keys",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU8yvqsbEzLbWLGzSdjosZAeYAfCx0DIjskTbbv1T1VbN-B5tmWg&s",
+        "scores": [
+            5,
+            2,
+            3,
+            4,
+            1,
+            2,
+            3,
+            4,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Ariana Grande",
+        "photo": "https://media1.popsugar-assets.com/files/thumbor/cv5OP-R3W-eyDn0GcGLLCJJWLmg/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2019/06/19/950/n/1922153/1e5067d35d0aadb3e2ecf9.02908622_/i/Best-Celebrity-Makeup-Artists.jpg",
+        "scores": [
+            5,
+            3,
+            1,
+            4,
+            3,
+            1,
+            2,
+            4,
+            3,
+            1
+        ]
+    }
+];
 
-module.exports = user;
-
-
-// Determine the user's most compatible friend using the following as a guide:
-
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-
-// Example:
-// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-// Total Difference: 2 + 1 + 2 = 5
-
-// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-
-// The closest match will be the user with the least amount of difference.
-// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-
-// The modal should display both the name and picture of the closest match.
+module.exports = userData;
