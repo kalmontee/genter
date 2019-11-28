7bae7878614088bfd18de3558d679138c57ad52a
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-28 13:54:10 -0500

    htmlRoutes connect successfully

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,16 +1,41 @@
 // Your apiRoutes.js file should contain two routes:
+const friends = require("../data/friends");
 
 module.exports = app => {
 
     // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-    var friends = require("../data/friends");
 
     app.get("/api/friends", function(req, res) {
-        res.json(user);
+        res.json(friends);
     });
 
     // A POST routes /api/friends. This will be used to handle incoming survey results.
-    // app.post("/api/friends", (req, res) => {
+    app.post("/api/friends", (req, res) => {
+        let matchFriend = req.body;
+        let newScores = matchFriend.scores;
+        // The closest match will be the user with the least amount of difference.
+        let differenceArr = [];
+        // By comparing all scores this will hold the best match
+        let matchIndex = 0;
 
-    // });
+        friends.forEach(element => {
+            var friendScores = element.friends.scores;
+            var scoreDifference = 0;
+
+            newScores.forEach(product => {
+                scoreDifference += Math.abs(parseInt(product.newScores) - parseInt(product.newScores));
+            });
+            differenceArr.push(scoreDifference);
+
+        });
+
+        differenceArr.forEach(element => {
+            if (element.differenceArr <= differenceArr[matchIndex]) {
+                matchIndex = element;
+            }
+        });
+
+        friends.push(matchFriend);
+        return res.json(friends[matchIndex]);
+    });
 }
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,14 +1,18 @@
-var path = require("path");
+const path = require("path");
 
 module.exports = app => {
 
     // A default, catch-all route that leads to home.html which displays the home page.
-    app.get("*", function(req, res) {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
+    app.get("assets", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/assets/images"))
+    });
+
     // A GET Route to /survey which should display the survey page.
-    app.get("/survey", function(req, res) {
+    app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
