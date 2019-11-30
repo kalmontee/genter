0bf14a4d599083d4b7669cbcbfed51c47badfb89
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-30 17:48:50 -0500

    Removed the assets app.get line of code.

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -7,10 +7,6 @@ module.exports = app => {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.get("assets", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/assets/images"))
-    });
-
     // A GET Route to /survey which should display the survey page.
     app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
